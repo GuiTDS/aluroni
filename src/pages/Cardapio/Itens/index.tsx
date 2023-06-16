333bb6a83f76832d1424fe1e830e0d9f55caa03e
Guilherme Sanches <guitdsanches@gmail.com>
2023-06-15 21:13:06 -0300

    adding filters to menu

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -11,22 +11,35 @@ interface Props {
 
 const Itens = (props:Props) => {
     const [lista, setLista] = useState(cardapio);
-    const { busca, filtro } = props;
+    const { busca, filtro, ordenador } = props;
 
-    function testaBusca(title: string) {
-        const regex = RegExp(busca, 'i');
-        return regex.test(title);
-    }
-
-    function testaFiltro(id: number | null) {
-        if(filtro !== null) return filtro === id; 
-        return true;
-    }
 
     useEffect(() => {
+        function testaBusca(title: string) {
+            const regex = RegExp(busca, 'i');
+            return regex.test(title);
+        }
+    
+        function testaFiltro(id: number | null) {
+            if(filtro !== null) return filtro === id; 
+            return true;
+        }
+
+        function ordenar(novaLista: typeof cardapio) {
+            switch(ordenador) {
+              case 'porcao': 
+                return novaLista.sort((a, b) => a.size > b.size ? 1 : -1);
+              case 'qtd_pessoas':
+                return novaLista.sort((a,b) => a.serving > b.serving ? 1 : -1);
+              case 'preco':
+                return novaLista.sort((a,b) => a.price > b.price ? 1 : -1);
+              default:
+                return novaLista; 
+            }
+          }
         const novaLista = cardapio.filter(item => testaBusca(item.title) && testaFiltro(item.category.id));
-        setLista(novaLista);
-    }, [busca, filtro]);
+        setLista(ordenar(novaLista));
+    }, [busca, filtro, ordenador]);
 
     return ( 
         <div className={styles.itens}>
